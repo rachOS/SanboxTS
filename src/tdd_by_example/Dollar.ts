615b512a60efddd6b683a95c0601ae0d6ca3d471
rachOS <g.chamekh@e-nautia.com>
2021-11-25 19:41:03 +0100

    tests: equal()

--- src/tdd_by_example/Dollar.ts
+++ src/tdd_by_example/Dollar.ts
@@ -1,20 +1,14 @@
-/**
- * TODO
- * 5$ + 10 CHF = 10$ if rate is 2:1
- * - [x] 5$ * 2 = 10$
- * Make amount private
- * Dollar side-effect?
- * Money rounding
- */
-
 export class Dollar implements IDollarProperties {
   constructor(public amount: number = 0) {
     this.amount = amount;
   }
 
-  public times(multiplier: number): number {
-    return (this.amount *= multiplier);
-    // return new Dollar(this.amount * multiplier);
+  public times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
+  }
+
+  public equal(money: Dollar): boolean {
+    return money.amount === this.amount;
   }
 }
 
--- src/tdd_by_example/__tests__/Multiplication.test.ts
+++ src/tdd_by_example/__tests__/Multiplication.test.ts
@@ -1,11 +1,28 @@
-import { mocked } from 'ts-jest/utils';
 import { Dollar } from '../Dollar';
+
+/**
+ * TODO
+ * 5$ + 10 CHF = 10$ if rate is 2:1
+ * - [x] 5$ * 2 = 10$
+ * Make amount private
+ * - [x] Dollar side-effect?
+ * - [x] equals()
+ * Money rounding
+ */
 
 describe('Dollar', () => {
   //let product: Dollar;
-  it('my money value should be multiplied by the given amount', () => {
-    const money: Dollar = new Dollar(5);
-    money.times(2);
-    expect(money.amount).toEqual(10);
+  it('money value should be multiplied by the given amount', () => {
+    const money = new Dollar(5);
+    let product = money.times(2);
+    expect(product.amount).toEqual(10);
+    product = money.times(5);
+    expect(product.amount).toEqual(25);
+  });
+
+  it('two money with same amount should be equal', () => {
+    let money = new Dollar(5);
+    expect(money.equal(new Dollar(5))).toBeTruthy();
+    expect(money.equal(new Dollar(6))).toBeFalsy();
   });
 });
