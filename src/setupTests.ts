1fa9e2ef3f74bde0cb8354a92b926b913fb28e27
rachOS <g.chamekh@e-nautia.com>
2021-12-18 08:26:38 +0100

    config : setup coverage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+module.exports = {
+    setupFilesAfterEnv: '<rootDir>/jest.config.ts',
+  };
+  
