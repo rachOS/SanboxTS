33b535e9e9b23da468c697ebf13e072a00e8f0eb
rachOS <g.chamekh@e-nautia.com>
2022-01-04 22:39:15 +0100

    tests : Generator

--- src/generatorFn/Generator.test.ts
+++ src/generatorFn/Generator.test.ts
@@ -1,18 +1,36 @@
 import { Generator } from './Generator';
 
-describe('', () => {
-  it('should add an item to the basket when asked', () => {
-    const generator = new Generator();
+let generator: Generator;
 
+describe('', () => {
+  beforeEach(() => {
+    generator = new Generator();
+  });
+  it('should add an item to the basket when asked', () => {
     expect(generator.getBasket()).toHaveLength(0);
-    generator.add<number>(42);
+
+    generator.add(42).next();
     expect(generator.getBasket()).toHaveLength(1);
-    generator.add({ item: 'banane' });
+
+    generator.add({ item: 'banane' }).next();
     expect(generator.getBasket()).toEqual([42, { item: 'banane' }]);
+
+    expect(generator.add('test').next()).toEqual({
+      done: false,
+      value: 3,
+    });
+
+    expect(generator.add('test').return([])).toEqual({
+      done: true,
+      value: [],
+    });
   });
 
-  it('should had item when asked', () => {
-    //generator.add<number>(42);
-    //expect(generator.getBasket()).toHaveLength(1);
+  it('should clear my basket when asked', () => {
+    generator.add(42).next();
+    expect(generator.getBasket()).toHaveLength(1);
+
+    generator.clear();
+    expect(generator.getBasket()).toHaveLength(0);
   });
 });
--- src/generatorFn/Generator.ts
+++ src/generatorFn/Generator.ts
@@ -1,12 +1,17 @@
 export class Generator {
   private readonly basket: Unknown = [];
+  *add(item: unknown): Generator<number, Unknown, unknown> {
+    yield this.basket.push(item);
 
-  add<T>(item: T): void {
-    this.basket.push(item);
+    return this.basket;
   }
 
   getBasket(): Unknown {
     return this.basket;
+  }
+
+  clear(): void {
+    this.basket.length = 0;
   }
 }
 
